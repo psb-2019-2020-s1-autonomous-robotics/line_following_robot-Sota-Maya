273fc9101f6e4f84f7ad8f283086fdcb4b58e961
may80914 <57004560+may80914@users.noreply.github.com>
2020-01-03 09:28:27 -0500

    Mostly working.

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ from pybricks.robotics import DriveBase
 
 
 brick.sound.beep()
+
 front_motor = Motor(Port.A)
 motorB = Motor(Port.B)
 color_sensor = ColorSensor(Port.S1)
 deciding_value = 40
 while True:
     if color_sensor.reflection() < deciding_value:
+        brick.display.text("Sensed line")
         motorB.reset_angle(0)
-        motorB.run_target(500, 1800)
+        motorB.run_target(500, -90)
     else:
+        brick.display.text("Nothing sensed, searching.")
         a = 5
-        while color_sensor.reflection() < deciding_value:
-            front_motor.run_target(500,a)
-            front_motor.run_target(500, -a)
-        front_motor.run_target(500,0)
-    brick.sound.beep()
+        b = 1
+        while color_sensor.reflection() > deciding_value:
+            front_motor.run_target(500,a*b)
+            a = a + 2.5
+            b = - b
+        front_motor.run_target(500,0) 
+    #brick.display.clear()
+    #brick.sound.beep()
