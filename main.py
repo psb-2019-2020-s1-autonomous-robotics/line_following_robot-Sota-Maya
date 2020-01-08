cfe29fe613a7e7c23166355ca03bebb9a974f29c
may80914 <57004560+may80914@users.noreply.github.com>
2020-01-08 09:26:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from pybricks.robotics import DriveBase
 
 brick.sound.beep()
 #Sets up sensor and motors
-moving_motor = Motor(Port.A)
-rotating_motor = Motor(Port.B)
+moving_motor = Motor(Port.B)
+rotating_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S1)
-
+print("Set up motors and sensor")
+#Get the deciding value
 while Button.LEFT not in brick.buttons():
     wait(1)
 white = color_sensor.reflection()
@@ -24,11 +25,15 @@ while Button.RIGHT not in brick.buttons():
 black = color_sensor.reflection()
 brick.sound.beep()
 deciding_value = (white + black)/2
+print("Deciding value = %s, white = %s. black = %s" % (deciding_value, white, black))
 while True:
     if color_sensor.reflection() < deciding_value: #Checks to see if the robot is on line
         brick.display.text("%s Sensed line" % color_sensor.reflection())
+        print("%s Sensed line" % color_sensor.reflection())
         moving_motor.reset_angle(0)
+        print("reset angle")
         moving_motor.run_target(500, -90) #Moves robot forward
+        print("moved forward")
     else: 
         brick.display.text("%s Nothing sensed, searching." % color_sensor.reflection())
         incrament = 2
@@ -38,3 +43,6 @@ while True:
             rotating_motor.run_target(500,a*b)
             a = a + incrament
             b = - b
+            print("searching")
+        print("end of else")
+    print("end of while")
